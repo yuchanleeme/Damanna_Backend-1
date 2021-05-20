8693a38e560cff9677df9ddb602e438b036e854c
yuchanleeme <yuchanlee.me@gmail.com>
2021-05-20 16:47:16 +0900

    [FEAT] socket setting

---
+++ src/main/java/com/th/damanna/controller/WebSocketEventListener.java
@@ -0,0 +1,41 @@
+package com.th.damanna.controller;
+
+import com.th.damanna.model.ChatMessage;
+import com.th.damanna.model.MessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+@Component
+public class WebSocketEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event){
+        logger.info("새로운 연결을 받았습니다.");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionConnectedEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+            logger.info(username + "유저의 연결이 끊겼습니다.");
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(MessageType.LEAVE);
+            chatMessage.setSender(username);
+
+            messageTemplate.convertAndSend("/topic/apple",chatMessage);
+        }
+    }
+}
---
+++ src/main/java/com/th/damanna/model/MessageType.java
@@ -0,0 +1,7 @@
+package com.th.damanna.model;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
