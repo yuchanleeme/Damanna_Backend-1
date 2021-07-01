f3c1d314b84100df6785f64c123b636378ff3df4
yuchanleeme <yuchanlee.me@gmail.com>
2021-07-01 15:29:41 +0900

    [REFACTOR] 불필요 코드 제거 및 주석 작성

--- src/main/java/com/th/damanna/controller/ChatController.java
+++ src/main/java/com/th/damanna/controller/ChatController.java
@@ -11,24 +11,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat/sendMessage/{topicID}")
-    @SendTo("/topic/{topicID}")
-    public ChatMessage sendMessage(@DestinationVariable String topicID, @Payload ChatMessage chatMessage){
-        // topicID 별로 나눔
-        System.out.println("topicID = " + topicID);
+    @MessageMapping("/chat/sendMessage/{roomID}")
+    @SendTo("/topic/{roomID}")
+    public ChatMessage sendMessage(@DestinationVariable String roomID, @Payload ChatMessage chatMessage){
+        // roomID 별로 나눔
+        System.out.println("topicID = " + roomID);
         System.out.println("chatMessage = " + chatMessage.getContent());
-        return chatMessage;
-    }
-
-    @MessageMapping("/chat/addUser/{topicID}")
-    @SendTo("/topic/{topicID}")
-    public ChatMessage addUser(@DestinationVariable String topicID, @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
-        // topicID 별로 나눔
-        System.out.println("topicID = " + topicID);
-        System.out.println("Sender = " + chatMessage.getSender());
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        headerAccessor.getSessionAttributes().put("topicID", topicID);
-
-        return chatMessage;
+        return chatMessage; // 채팅 메세지 리턴
     }
 }
